74d57483fe0c83ca74ea8d3acdcb829973a116bd
gbs4ever <gbs4ever2@gmail.com>
2020-01-17 14:12:38 -0500

    setting up continuous integration

---
+++ src/components/ShulInfo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class ShulInfo extends React.Component {
+  render() {
+    return (
+      <div>
+        Membership for our bais medrash is $750 a year. Checks can be made out
+        to Cong Torah Utfilla, or to pay by credit card{' '}
+        <a href="#">click here</a>. To set up recurring monthly payments, please
+        click on the "Membership Fees" button below.
+      </div>
+    )
+  }
+}
+
+export default ShulInfo
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,41 @@
-import React from "react";
-import "../assets/scss/main.scss";
-
-import Header from "./Header";
-import Footer from "./Footer";
+import React from 'react'
+import '../assets/scss/main.scss'
+import ShulInfo from './ShulInfo.js'
+import Header from './Header'
+import Footer from './Footer'
 
 class Template extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        loading: 'is-loading'
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: 'is-loading',
     }
+  }
 
-    componentDidMount () {
-      this.timeoutId = setTimeout(() => {
-          this.setState({loading: ''});
-      }, 100);
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => {
+      this.setState({ loading: '' })
+    }, 100)
+  }
+
+  componentWillUnmount() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId)
     }
+  }
 
-    componentWillUnmount () {
-      if (this.timeoutId) {
-          clearTimeout(this.timeoutId);
-      }
-    }
+  render() {
+    const { children } = this.props
 
-    render() {
-        const { children } = this.props;
-
-        return (
-            <div className={`body ${this.state.loading}`}>
-                <Header />
-                {children}
-                <Footer />
-            </div>
-        );
-    }
+    return (
+      <div className={`body ${this.state.loading}`}>
+        <Header />
+        <ShulInfo />
+        {children}
+        <Footer />
+      </div>
+    )
+  }
 }
 
-export default Template;
+export default Template
