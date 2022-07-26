96ed4075c47a41e60fb69bfe37e3e4806ca395b2
gbs4ever <gbs4ever2@gmail.com>
2022-07-25 23:20:42 -0400

    fixed css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
-
+import entrance from '../assets/images/entrance.jpeg'
 import Zmanim from './Zmanim'
+// let sectionStyle = {
+//   width: "100%",
+//   height: "400px",
+//   backgroundImage: `url(${entrance})`
+// };
 class Header extends React.Component {
   constructor(props) {
     super(props)
@@ -22,38 +27,37 @@ class Header extends React.Component {
   }
   render() {
     return (
-      <div className="wrapper">
-        <div className={`body ${this.state.loading}`}>
-          <section id="header">
-            <Zmanim />
-            <div className="inner">
-              {/* <span className="icon major fa-cloud"></span> */}
-
-              <ul className="actions">
-                <li>
-                  <a href="#two" className="button scrolly">
-                    Donations & Dedications
-                  </a>
-                </li>
-                <li>
-                  <a href="#three" className="button scrolly">
-                    Zmanim
-                  </a>
-                </li>
-
-                <li>
-                  <a href="#five" className="button scrolly">
-                    Shul Information
-                  </a>
-                </li>
-                <li>
-                  <a href="#four" className="button scrolly">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </section>
+      <div className="welcomer">
+        <div className="wrapper">
+          <div className={`body ${this.state.loading}`}>
+            <section id="header">
+              <Zmanim />
+              <div className="inner">
+                <ul className="actions">
+                  <li>
+                    <a href="#two" className="button scrolly">
+                      Donations & Dedications
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#three" className="button scrolly">
+                      Zmanim
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#five" className="button scrolly">
+                      Shul Information
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#four" className="button scrolly">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     )
