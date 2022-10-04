c927c56b41e2b39138c21cd68a7fe0d2ba8e4286
gbs4ever <gbs4ever2@gmail.com>
2022-10-03 20:57:10 -0400

    fix and remove pic

--- src/components/ShulInfo.js
+++ src/components/ShulInfo.js
@@ -3,16 +3,21 @@ import React from 'react'
 class ShulInfo extends React.Component {
   render() {
     return (
-      <div className="actions">
-        Please take this opportunity to help make our kehilla and shul complete.
-        Membership for our bais medrash is $750 a year.<br></br>Payment can be
-        made by check made out to Cong Torah Utfilla.
-        <br></br> To pay by credit card or to set up recurring monthly payments.
-        <br></br>
-        <a href="https://secure.cardknox.com/torahutfilla" className="button">
-          click here
-        </a>{' '}
-        .
+      <div>
+        <div className="first-row">
+          Please take this opportunity to help make our kehilla and shul
+          complete. Membership for our bais medrash is $750 a year.<br></br>
+          Payment can be made by check made out to Cong Torah Utfilla.
+          <br></br> To pay by credit card or to set up recurring monthly
+          payments.
+          <br></br>
+          <a href="https://secure.cardknox.com/torahutfilla" className="button">
+            click here
+          </a>{' '}
+        </div>
+        <div className="second-row">
+          <iframe src="https://secure.cardknox.com/torahutfilla" />
+        </div>
       </div>
     )
   }
