5f51272b23dcbc8b532dd9af68352fe2de55ff70
gbs4ever <gbs4ever2@gmail.com>
2022-10-23 13:41:52 -0400

    added picture for page 2

--- src/pages/building.js
+++ src/pages/building.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
-
+import main from '../assets/images/main.jpg'
 export default function Index() {
   return (
     <div>
       Congregation Bais Torah Utefilah of Chestnut Ridge under work
       ...............
+      <div className="col-4">
+        <span className="image fit">
+          <img src={main} alt="750 shul " />
+        </span>
+      </div>
     </div>
   )
 }
