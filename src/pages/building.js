477eeaeaf01af8bb49a50bb049bcf9340b3dfacd
gbs4ever <gbs4ever2@gmail.com>
2022-10-23 14:04:21 -0400

    padding top for pics

--- src/pages/building.js
+++ src/pages/building.js
@@ -1,12 +1,12 @@
 import * as React from 'react'
-import main from '../assets/images/main.jpeg'
+import main from '../assets/images/Dedication Opportunities.jpg'
 export default function Index() {
   return (
-    <div>
+    <div className="main style3 special">
       Congregation Bais Torah Utefilah of Chestnut Ridge under work
       ...............
       <div className="col-4">
-        <span className="image fit">
+        <span className="image_center">
           <img src={main} alt="750 shul " />
         </span>
       </div>
