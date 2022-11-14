52e135dfb12ab85697f84055d43e2189a7ef07e4
gbs4ever <gbs4ever2@gmail.com>
2022-11-13 19:01:05 -0500

    pic center

--- src/pages/building.js
+++ src/pages/building.js
@@ -3,13 +3,7 @@ import main from '../assets/images/Dedication Opportunities.jpg'
 export default function Index() {
   return (
     <div className="main style3 special">
-      Congregation Bais Torah Utefilah of Chestnut Ridge under work
-      ...............
-      <div className="col-4">
-        <span className="image_center">
-          <img src={main} alt="750 shul " />
-        </span>
-      </div>
+      <img src={main} alt="750 shul " />
     </div>
   )
 }
