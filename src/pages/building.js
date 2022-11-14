d7082b783d7ab3ef38647d66203fbcc556505fbd
gbs4ever <gbs4ever2@gmail.com>
2022-11-13 19:15:39 -0500

    stretch pic

--- src/pages/building.js
+++ src/pages/building.js
@@ -3,9 +3,9 @@ import main from '../assets/images/Dedication Opportunities.jpg'
 export default function Index() {
   return (
     <div className="main style3 special">
-      <img src={main} alt="750 shul " />
+      <img className="bg" src={main} alt="750 shul " />
       <iframe
-        src="https://www.w3schools.com"
+        src="https://secure.cardknox.com/congtorahutfillakerenhabinyen"
         title="W3Schools Free Online Web Tutorials"
       ></iframe>
     </div>
