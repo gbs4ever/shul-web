a99bbbefa398b399e8aed091855fef2020739e9d
gbs4ever <gbs4ever2@gmail.com>
2022-11-13 19:10:26 -0500

    change links

--- src/pages/building.js
+++ src/pages/building.js
@@ -4,6 +4,10 @@ export default function Index() {
   return (
     <div className="main style3 special">
       <img src={main} alt="750 shul " />
+      <iframe
+        src="https://www.w3schools.com"
+        title="W3Schools Free Online Web Tutorials"
+      ></iframe>
     </div>
   )
 }
