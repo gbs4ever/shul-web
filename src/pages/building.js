79101f6cb19ff814ea1920da6a657fa7617c6451
gbs4ever <gbs4ever2@gmail.com>
2022-11-13 19:18:12 -0500

    stretch pic

--- src/pages/building.js
+++ src/pages/building.js
@@ -2,9 +2,11 @@ import * as React from 'react'
 import main from '../assets/images/Dedication Opportunities.jpg'
 export default function Index() {
   return (
-    <div className="main style3 special">
+    <div>
       <img className="bg" src={main} alt="750 shul " />
       <iframe
+        width="50%"
+        height="100%"
         src="https://secure.cardknox.com/congtorahutfillakerenhabinyen"
         title="W3Schools Free Online Web Tutorials"
       ></iframe>
