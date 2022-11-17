6c104938d6b97f577e894edb7dffbc5d9025a902
gbs4ever <gbs4ever2@gmail.com>
2022-11-16 23:23:29 -0500

    switch to backbround img

--- src/pages/building.js
+++ src/pages/building.js
@@ -2,7 +2,14 @@ import * as React from 'react'
 import background from '../assets/images/shul_Quality.jpg'
 export default function Index() {
   return (
-    <div style={{ backgroundImage: `url(${background})` }}>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: 'no-repeat',
+        width: '250px',
+        height: '100%',
+      }}
+    >
       <button class="open-button" onclick="openForm()">
         Open Form
       </button>
