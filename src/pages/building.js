bb38525db134686a8dd97c156fd72af3c112ae60
gbs4ever <gbs4ever2@gmail.com>
2022-11-16 23:18:46 -0500

    switch to backbround img

--- src/pages/building.js
+++ src/pages/building.js
@@ -1,15 +1,11 @@
 import * as React from 'react'
-import main from '../assets/images/shul_Quality.jpg'
+import background from '../assets/images/shul_Quality.jpg'
 export default function Index() {
   return (
-    <div className="pics">
-      <img src={main} alt="750 shul " />
-      {/* <iframe
-        width="50%"
-        height="100%"
-        src="https://secure.cardknox.com/congtorahutfillakerenhabinyen"
-        title="W3Schools Free Online Web Tutorials"
-      ></iframe> */}
+    <div style={{ backgroundImage: `url(${background})` }}>
+      <button class="open-button" onclick="openForm()">
+        Open Form
+      </button>
     </div>
   )
 }
